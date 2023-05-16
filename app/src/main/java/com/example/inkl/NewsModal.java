de37f2ff6ceaf3455b602990dbe86f337f1c0608
kratikagupta0306 <92164742+kratikagupta0306@users.noreply.github.com>
2023-05-16 16:29:12 +0530

    create Articles.java and NewsModal.java

--- app/src/main/java/com/example/inkl/NewsModal.java
+++ app/src/main/java/com/example/inkl/NewsModal.java
@@ -1,4 +1,33 @@
 package com.example.inkl;
 
 public class NewsModal {
+    private int TotalResults;
+    private String status;
+    private ArrayList<Articles> articles;
+
+    public NewsModal(int totalResults, String status, ArrayList<Articles> articles) {
+        this.TotalResults = totalResults;
+        this.status = status;
+        this.articles = articles;
+    }
+
+    public int getTotalResults() {
+        return TotalResults;
+    }
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+    public ArrayList<Articles> getArticles() {  
+        return articles;
+    }
+    public void setArticles(ArrayList<Articles> articles){
+        this.articles = articles;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
